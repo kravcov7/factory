3c4b19957290ce4dccc311c7223024babc3a20ca
Artem K <a.kravtsov@mish.design>
2023-12-06 21:30:05 +0300

    create reacte app

---
+++ src/comonents/component1/Component1.jsx
@@ -0,0 +1,11 @@
+// import { useTranslation } from 'react-i18next';
+// import { classNames } from 'shared/lib/classNames/classNames';
+// import { Button, ThemeButton } from 'shared/ui/Button/Button';
+
+export const Component1 = () => {
+  return (
+    <div className="">
+      <h1>Component 1</h1>
+    </div>
+  );
+};
---
+++ src/comonents/component2/Component2.jsx
@@ -0,0 +1,10 @@
+
+
+export const Component2 = () => {
+    return (
+      <div className="">
+        <h1>Component 1</h1>
+      </div>
+    );
+  };
+  
