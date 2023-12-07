f98d3b0b2e7adca80ad0f37fb6bc1fbc8beb0cb1
Artem K <a.kravtsov@mish.design>
2023-12-07 17:00:43 +0300

    edit localStorage

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,3 @@
-'use client'
 
 import Head from "next/head";
 import { Inter } from "next/font/google";
@@ -8,20 +7,19 @@ import CompletedList from "@/components/component2/CompletedList";
 import TaskForm from "@/components/TaskForm/TaskForm";
 import React, { useState, useEffect } from "react";
 import { v4 as uuidv4 } from "uuid";
-import useLocalStorage from "@/utils/hooks/useLocalStorage"
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
-  // let value
-  // value = localStorage.getItem("tasks") || []
-  // const [value, setValue] = useLocalStorage(tasks, []);
-  // const [tasks, setTasks] = useLocalStorage(value);
   const [tasks, setTasks] = useState([]);
   const [completedTasks, setCompletedTasks] = useState([]);
 
   const addTask = (newTask) => {
     setTasks([...tasks, { ...newTask, id: uuidv4() }]);
+    localStorage.setItem(
+      "tasks",
+      JSON.stringify([...tasks, { ...newTask, id: uuidv4() }])
+    );
   };
 
   const completeTask = (taskId) => {
@@ -29,36 +27,35 @@ export default function Home() {
     const completedTask = tasks.find((task) => task.id === taskId);
     setCompletedTasks([...completedTasks, completedTask]);
     setTasks(tasks.filter((task) => task.id !== taskId));
+    localStorage.setItem(
+      "completedTasks",
+      JSON.stringify([...completedTasks, completedTask])
+    );
+    localStorage.setItem(
+      "tasks",
+      JSON.stringify(tasks.filter((task) => task.id !== taskId))
+    );
   };
 
   const deleteTask = (taskId) => {
     // clearInterval(timers[taskId]);
+    localStorage.setItem(
+      "tasks",
+      JSON.stringify(tasks.filter((task) => task.id !== taskId))
+    );
     setTasks(tasks.filter((task) => task.id !== taskId));
   };
 
   const clearCompleted = () => {
+    localStorage.removeItem("completedTasks");
+
     setCompletedTasks([]);
   };
-
-  
-  useEffect(() => {
-    localStorage.setItem("tasks", JSON.stringify(tasks));
-  }, [tasks]);
-  
-  useEffect(() => {
-    localStorage.setItem("completedTasks", JSON.stringify(completedTasks));
-  }, [completedTasks]);
 
   useEffect(() => {
     const storedTasks = JSON.parse(localStorage.getItem("tasks")) || [];
     const storedCompletedTasks =
       JSON.parse(localStorage.getItem("completedTasks")) || [];
-
-    console.log("Loaded tasks from localStorage:", storedTasks);
-    console.log(
-      "Loaded completed tasks from localStorage:",
-      storedCompletedTasks
-    );
 
     setTasks(storedTasks);
     setCompletedTasks(() => storedCompletedTasks);
